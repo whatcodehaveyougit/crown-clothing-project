1037c401c70f279a965014090528ab8af1450e50
whatcodehaveyougit <siggywatt@yahoo.com>
2022-09-14 13:19:57 +0200

    added shop component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './routes/home/home.component'
 import Navigation from './routes/navigation/navigation.component'
 import Authentication from './routes/authentication/authentication.component'
+import Shop from '../src/routes/shop/shop.component'
 
 const App = () => {
 
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
