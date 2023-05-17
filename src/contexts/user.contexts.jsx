0b97b8cf39565c59fcbe847a12c76edb48a6b184
sigurdwatt <sigurdwatt@sigurd.watt>
2023-05-17 12:03:15 +0200

    Add Reducer to userProvider and taken out context

--- src/contexts/user.contexts.jsx
+++ src/contexts/user.contexts.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
@@ -10,9 +10,40 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  console.log(action);
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type}`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 // This is the actual component
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  console.log(currentUser);
+  console.log("dispatch");
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   // Centralising control of setting user, best here rather than on sign in/out component
