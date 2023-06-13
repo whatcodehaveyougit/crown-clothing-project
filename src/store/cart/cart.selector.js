3ae04a4f50af9eac6e12dcabf0756661c8f8ef3c
whatcodehaveyougit <sigurdwatt@gmail.com>
2023-06-13 12:50:07 +0200

    Fix checkout bug

--- src/store/cart/cart.selector.js
+++ src/store/cart/cart.selector.js
@@ -19,6 +19,6 @@ export const selectCartCount = createSelector([selectCartItems], (cartItems) =>
 export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce(
     (total, cartItem) => total + cartItem.quantity * cartItem.price,
-    []
+    0
   )
 );
